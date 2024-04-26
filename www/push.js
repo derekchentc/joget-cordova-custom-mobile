8508d0908d15ea00ed6ef889c547cdd92a99d3e1
Derek Chen <62978237+derekchentc@users.noreply.github.com>
2024-04-26 14:20:33 +0800

    change push notification url

--- www/push.js
+++ www/push.js
@@ -1,7 +1,7 @@
 var MobilePush = {
     
     CONFIG_SENDER_ID: "151651598697",
-    CONFIG_PUSH_SERVER_URL: "https://mobile.cloud.joget.com/jw/web/json/app/jms/plugin/org.joget.mobile.MobilePushPlugin/service",
+    CONFIG_PUSH_SERVER_URL: "http://localhost:8080/jw/web/json/app/jms/plugin/org.joget.mobile.MobilePushPlugin/service",
     pushRegistrationId: "",
 
     init: function() {
